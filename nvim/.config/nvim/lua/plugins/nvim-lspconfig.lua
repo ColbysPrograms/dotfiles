ef43cb6d5f44b08a81cea68a28c1232f86b1f7ce
Colby <colby.cox2020@gmail.com>
2025-05-26 01:06:53 -0500

    commit

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,14 +1,11 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    optional = true,
     opts = {
-      servers = {
-        texlab = {
-          keys = {
-            { "<Leader>K", "<plug>(vimtex-doc-package)", desc = "Vimtex Docs", silent = true },
-          },
-        },
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
       },
     },
   },
